f622847693b6c975a45dcb175131bf6cdd61248f
lakkshyag <lakkshyag@gmail.com>
2022-11-18 01:40:33 +0530

    initial commit: added the intro and index file for this repo

---
+++ intro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello! This is the intro file for this repository. Refer to index.txt for more details.";
+
+    return 0;
+}
